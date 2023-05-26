391f428f1661d7258af6603647bfb0d06a56fcb6
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-26 14:22:08 +0300

    models and admin settings have been added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='prod_id')
+    name = models.CharField(max_length=100, verbose_name='prod_name')
+    description = models.TextField(verbose_name='prod_descr', **NULLABLE)
+    price = models.IntegerField(verbose_name='prod_price')
+    stock = models.IntegerField(verbose_name='prod_stock')
+    img = models.ImageField(upload_to='media/', verbose_name='product_img', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.id} {self.name}'
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
