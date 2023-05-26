1eaa5ad76e3e8d74a5e14233fdee7efb814a2e02
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-26 17:38:37 +0300

    text_limit_to_display custom tag's been added

--- main/templatetags/my_tags.py
+++ main/templatetags/my_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.templatetags.static import static
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,3 +9,14 @@ register = template.Library()
 @register.simple_tag
 def mediapath(path_to_pic):
     return static(path_to_pic)
+
+
+@register.filter(needs_autoescape=True)
+def text_limit_to_display(text, autoescape=True):
+    first_hundred = text[:100]
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    result = "%s" % (esc(first_hundred))
+    return mark_safe(result)
