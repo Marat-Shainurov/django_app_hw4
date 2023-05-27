3b1863c65a08e67a2575be7ee79f6fd095baf35d
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-27 15:52:47 +0300

    url update for both pages and inc_main_menu_and_carousel.html

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from main.apps import MainConfig
 from main.views import index, add_product
 
+app_name = MainConfig.name
+
 urlpatterns = [
-    path('', index),
-    path('add/', add_product)
+    path('', index, name='index'),
+    path('add/', add_product, name='add')
 ]
