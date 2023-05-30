7e5437298814afe6efa5f9ae10b631dec527cde2
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-30 17:36:55 +0300

    the CBV bersion's been added

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index, add_product, promo_two
+from main.views import PromoOneView, PromoTwoView, ProductListView, ProductCreateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('add/', add_product, name='add'),
-    path('promo_two/', promo_two, name='promo_two'),
-    path('promo_one/', promo_two, name='promo_one')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('add/', ProductCreateView.as_view(), name='add'),
+    path('promo_one/', PromoOneView.as_view(), name='promo_one'),
+    path('promo_two/', PromoTwoView.as_view(), name='promo_two')
 ]
--- main/views.py
+++ main/views.py
@@ -3,47 +3,73 @@ import os.path
 from django.core.files.storage import default_storage
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from config import settings
 from main.models import Product
 
 
-def index(request):
-    all_products = Product.objects.all()
+class ProductListView(generic.ListView):
+    model = Product
+    context_object_name = 'page_obj'
+    paginate_by = 3
+    page_title = 'Main page'
 
-    paginator = Paginator(all_products, 3)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page_title'] = self.page_title
+        return context
 
-    context = {
-        'page_obj': page_obj,
-        'page_title': 'Main page'
+
+#
+# def index(request):
+#     all_products = Product.objects.all()
+#
+#     paginator = Paginator(all_products, 3)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#
+#     context = {
+#         'page_obj': page_obj,
+#         'page_title': 'Main page'
+#     }
+#     return render(request, 'main/product_list.html', context)
+#
+
+
+class ProductCreateView(generic.CreateView):
+    model = Product
+    fields = ('name', 'description', 'price', 'stock', 'img')
+    success_url = reverse_lazy('main:product_list')
+#
+#
+# def add_product(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         description = request.POST.get('description')
+#         price = request.POST.get('price')
+#         stock = request.POST.get('stock')
+#         img = request.FILES.get('img')
+#
+#         new_product = Product(name=name, description=description, price=price, stock=stock, img=img)
+#         file_path = default_storage.save(f"media/{img}", img)
+#         new_product.img = file_path
+#         new_product.save()
+#
+#     context = {'page_title': 'Add a product'}
+#     return render(request, 'main/product_form.html', context)
+
+
+class PromoOneView(generic.TemplateView):
+    template_name = 'main/promo_page_1.html'
+    extra_context = {
+        'page_title': 'Promo one'
     }
-    return render(request, 'main/index.html', context)
 
 
-def add_product(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        price = request.POST.get('price')
-        stock = request.POST.get('stock')
-        img = request.FILES.get('img')
-
-        new_product = Product(name=name, description=description, price=price, stock=stock, img=img)
-        file_path = default_storage.save(f"media/{img}", img)
-        new_product.img = file_path
-        new_product.save()
-
-    context = {'page_title': 'Add a product'}
-    return render(request, 'main/add_product.html', context)
-
-
-def promo_one(request):
-    context = {'page_title': 'Promo one'}
-    return render(request, 'main/promo_page_1.html', context)
-
-
-def promo_two(request):
-    context = {'page_title': 'Promo two'}
-    return render(request, 'main/promo_page_2.html', context)
+class PromoTwoView(generic.TemplateView):
+    template_name = 'main/promo_page_2.html'
+    extra_context = {
+        'page_title': 'Promo two'
+    }
