da0229a2398feb5ae6315e26c33a1adea6f77a0a
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-31 22:13:23 +0300

    titles update

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,16 @@ from blog.models import Blog
 
 class BlogListView(generic.ListView):
     model = Blog
+    extra_context = {'page_title': 'Blogs'}
 
 
 class BlogDetailView(generic.DetailView):
     model = Blog
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['page_title'] = 'Blog details'
+        return context_data
 
 # class BlogCreateView(generic.CreateView):
 #     model = Blog
