ed813684a3c56390316776c361308b4bc2e27263
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-31 19:49:53 +0300

    list and detail contriller classes have been added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from blog.models import Blog
@@ -6,3 +7,23 @@ from blog.models import Blog
 
 class BlogListView(generic.ListView):
     model = Blog
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+
+# class BlogCreateView(generic.CreateView):
+#     model = Blog
+#     fields = None
+#     success_url = reverse_lazy('blog:blog_detail')
+
+#
+# class BlogUpdateView(generic.UpdateView):
+#     model = Blog
+#     fields = None
+#     success_url = reverse_lazy('blog:blog_detail')
+#
+#
+# class BlogDeleteView(generic.DetailView):
+#     model = Blog
+#     success_url = reverse_lazy('blog:blog_list')
