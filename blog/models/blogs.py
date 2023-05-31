2c38bec1210681e4c7f3dfcd32d62b411b7fe802
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-31 18:05:39 +0300

    the slug field's been defined

--- blog/models/blogs.py
+++ blog/models/blogs.py
@@ -1,17 +1,28 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Blog(models.Model):
-    heading = models.CharField(max_length=100, verbose_name='heading')
-    slug = models.CharField(max_length=100, verbose_name='slug')
+    heading = models.CharField(max_length=250, verbose_name='heading', unique=True)
+    slug = models.CharField(max_length=250, verbose_name='slug', **NULLABLE)
     content = models.TextField(verbose_name='content')
     img = models.ImageField(upload_to='blog/', verbose_name='preview')
     created = models.DateTimeField(auto_now_add=True, verbose_name='creation date')
     is_published = models.BooleanField(default=True, verbose_name='is_published')
-    views = models.IntegerField(verbose_name='views number')
+    views = models.IntegerField(verbose_name='views number', default=0)
 
     def __str__(self):
         return f'{self.heading} {self.slug}'
+
+    def save(self, *args, **kwargs):
+        slug = ''
+        for char in self.heading:
+            if char.isalnum() or char == ' ':
+                slug += char
+        self.slug = slug.replace(' ', '-')
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Blog'
