697eaf4a87051a26bba30fe5ad845b0629698700
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-31 12:48:21 +0300

    page titles update

--- main/views.py
+++ main/views.py
@@ -6,16 +6,21 @@ from main.models import Product
 
 class ProductListView(generic.ListView):
     model = Product
-    template_name = 'main/product_list.html'
-    context_object_name = 'object_list'
     paginate_by = 3
-    page_title = 'Main page'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page_title'] = 'Main page'
+        return context
 
 
 class ProductCreateView(generic.CreateView):
     model = Product
     fields = ('name', 'description', 'price', 'stock', 'img')
     success_url = reverse_lazy('main:product_list')
+    extra_context = {
+        'page_title': 'Add a product'
+    }
 
 
 class PromoOneView(generic.TemplateView):
