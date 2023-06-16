9101ad54ad55775223798d6aed2eb92cbb3e74c2
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-16 13:06:18 +0300

    adds the login and logout functional

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('', include('blog.urls', namespace='blog'))
+    path('', include('blog.urls', namespace='blog')),
+    path('', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
