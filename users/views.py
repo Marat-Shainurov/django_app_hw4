08d3f2f6c66c302ac46a73cc426f7262318c5019
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-16 19:43:06 +0300

    adds the register view's form_valid() method draft

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
+import random
+
+from django.conf import settings
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -20,3 +24,17 @@ class RegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        new_user = form.save()
+        if form.is_valid():
+            verification_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+            new_user.set_password(verification_password)
+            new_user.save()
+            send_mail(
+                'email verification',
+                f'Your password - {verification_password}.',
+                settings.EMAIL_HOST_USER,
+                [form.cleaned_data['email']]
+            )
+        return super().form_valid(form)
