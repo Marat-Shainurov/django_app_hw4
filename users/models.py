e36715461da77f2b1f14522964e89055cf2d718a
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-17 13:43:44 +0300

    adds email verification while registration

--- users/models.py
+++ users/models.py
@@ -10,6 +10,8 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatar/', verbose_name='user_avatar', **NULLABLE)
     phone = models.CharField(max_length=40, verbose_name='user_phone', **NULLABLE)
     country = models.CharField(max_length=150, verbose_name='user_country', **NULLABLE)
+    verification_code = models.CharField(verbose_name='email_verification_code', **NULLABLE)
+    is_email_verified = models.BooleanField(verbose_name='is_email_verified', default=False, **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ import random
 from django.conf import settings
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import generic
 
 from users.forms import RegisterForm
@@ -23,18 +25,34 @@ class RegisterView(generic.CreateView):
     model = User
     form_class = RegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('users:login')
 
     def form_valid(self, form):
-        new_user = form.save()
+        self.object = form.save()
         if form.is_valid():
-            verification_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
-            new_user.set_password(verification_password)
-            new_user.save()
+            code = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+            self.object.verification_code = code
+            self.object.save()
             send_mail(
                 'email verification',
-                f'Your password - {verification_password}.',
+                f'Your password - {code}. \nAssigned to new_user.verification_code - {self.object.verification_code}',
                 settings.EMAIL_HOST_USER,
-                [form.cleaned_data['email']]
+                [self.object.email]
             )
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('users:verify_email', kwargs={'email': self.object.email})
+
+
+def verify_email(request, email):
+    if request.method == 'POST':
+        code_to_check = request.POST.get('verification_code')
+        user = User.objects.get(email=email)
+        if user.verification_code == code_to_check:
+            user.is_email_verified = True
+            user.save()
+            return redirect(reverse('users:login'))
+        else:
+            raise ValidationError(f'You have used the wrong code!')
+    else:
+        return render(request, 'users/verify_email.html')
