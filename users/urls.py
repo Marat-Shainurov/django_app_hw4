c56453ed1fc12cc3a1d18a9fb8b80aac39f8d54b
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-17 14:08:00 +0300

    adds user restoration functional

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import LoginView, LogoutView, RegisterView, verify_email
+from users.views import LoginView, LogoutView, RegisterView, verify_email, restore_user
 
 app_name = UsersConfig.name
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('register/verification/<str:email>', verify_email, name='verify_email')
+    path('register/verification/<str:email>', verify_email, name='verify_email'),
+    path('login/restore/', restore_user, name='restore_user'),
 ]
--- users/views.py
+++ users/views.py
@@ -34,7 +34,7 @@ class RegisterView(generic.CreateView):
             self.object.save()
             send_mail(
                 'email verification',
-                f'Your password - {code}. \nAssigned to new_user.verification_code - {self.object.verification_code}',
+                f'Your password - {code}',
                 settings.EMAIL_HOST_USER,
                 [self.object.email]
             )
@@ -56,3 +56,21 @@ def verify_email(request, email):
             raise ValidationError(f'You have used the wrong code!')
     else:
         return render(request, 'users/verify_email.html')
+
+
+def restore_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('user_email')
+        user = User.objects.get(email=email)
+        new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+        send_mail(
+            'email verification',
+            f'Your new password - {new_password}. Use it for logging in.',
+            settings.EMAIL_HOST_USER,
+            [user.email]
+        )
+        user.set_password(new_password)
+        user.save()
+        return redirect(reverse('users:login'))
+    else:
+        return render(request, 'users/restore_password.html')
