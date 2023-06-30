13fbb606478d3e8d6b3d0477d71724dfcaf6d737
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-30 10:42:31 +0300

    updates get_versions with the cache_products func

--- main/services.py
+++ main/services.py
@@ -1,4 +1,22 @@
-from main.models import Product, Version
+from django.conf import settings
+from django.core.cache import cache
+
+from main.models import Version, Product
+
+
+def cache_products():
+
+    if settings.CACHE_ENABLED:
+        key = 'products_list'
+        products_list = cache.get(key)
+        if products_list is None:
+            products_list = Product.objects.all()
+            cache.set(key, products_list)
+        return products_list
+    else:
+        products_list = Product.objects.all()
+
+    return products_list
 
 
 def get_products_active(queryset):
