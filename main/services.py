aaa9c647228059ded6c64f87275ec2c2ec3b8635
Marat-Shainurov <m_shainurov@mail.ru>
2023-06-30 10:35:36 +0300

    updates get_versions

--- main/services.py
+++ main/services.py
@@ -1,9 +1,9 @@
-from main.models import Product
+from main.models import Product, Version
 
 
 def get_products_active(queryset):
     return queryset.filter(is_active=True)
 
 
-def get_versions(queryset, prod):
-    return queryset.filter(product=prod)
+def get_versions_by_prod(prod):
+    return Version.objects.filter(product=prod)
